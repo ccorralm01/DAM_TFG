42a73b6f5af22fa6d5c7d2765296d1e3a9d4ac83
ccorralm01 <ccorralm01@gmail.com>
2025-03-27 16:41:03 +0100

    Login rework

--- src/components/loginForm/LoginForm.tsx
+++ src/components/loginForm/LoginForm.tsx
@@ -4,27 +4,15 @@ import AnimatedBackground from '../background/AnimatedBackground';
 export default function LoginForm() {
     return (
         <div className="login-page-container">
-            <AnimatedBackground/>
-            <div className="login-container">
-                <div className="wrapper">
-                    <div className="title"><span>Inicia sesión</span></div>
-                    <form action="#">
-                        <div className="row">
-                            <i className="fas fa-user"></i>
-                            <input type="text" placeholder="Email or Phone" required />
-                        </div>
-                        <div className="row">
-                            <i className="fas fa-lock"></i>
-                            <input type="password" placeholder="Password" required />
-                        </div>
-                        <div className="pass"><a href="#">¿Olvidaste tu contraseña?</a></div>
-                        <div className="row button">
-                            <input type="submit" value="Login" />
-                        </div>
-                        <div className="signup-link">¿No tienes cuenta? <a href="#">Registrate</a></div>
-                    </form>
-                </div>
+            <AnimatedBackground />
+            <div className="container">
+                <form className="login">
+                    <span>logo</span>
+                    <input type="text" placeholder="Username" />
+                    <input type="password" placeholder="Password" />
+                    <button>Iniciar sesión</button>
+                </form>
             </div>
         </div>
-    );
+                );
 }
